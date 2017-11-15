149401b8afc529cde7e57c04da5fdb016fd9ac8a
“Vilde <vilde@vevatne.no>
2017-11-15 22:33:18 +0100

    Rewrite to functional components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+
+import { BrowserRouter, Match, Miss } from "react-router";
 import NavBar from "./components/NavBar";
 import Description from "./components/Description";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <NavBar />
-        <Description />
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Match exactly pattern="/" component={Description} />
+        <Match pattern="/contact" component={Contact} />
+        <Miss component={NotFound} />
       </div>
-    );
-  }
-}
+    </BrowserRouter>
+  );
+};
 
 export default App;
